4f2242664bdbe8b33521c216c3002c6536155742
Seynox <seynoxthegentleman@gmail.com>
2022-06-16 17:16:19 +0200

    Initial Commit

---
+++ saejinaapp/src/main/java/fr/seynox/saejinaapp/SaejinaappApplication.java
@@ -0,0 +1,13 @@
+package fr.seynox.saejinaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaejinaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaejinaappApplication.class, args);
+	}
+
+}
---
+++ saejinaapp/src/test/java/fr/seynox/saejinaapp/SaejinaappApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.seynox.saejinaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaejinaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
